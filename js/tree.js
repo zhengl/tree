1c78924be1dac0ad784e424395ed314048f36ac9
Zheng Li <loylizh@gmail.com>
2013-10-12 16:47:14 +0800

    Add DnD

--- js/tree.js
+++ js/tree.js
@@ -1,11 +1,41 @@
 +function ($) { "use strics";
 
-	var Tree = function (element) {
+	var Tree = function (element, options) {
 		this.$element = $(element)
+		this.options = $.extend({}, Tree.DEFAULTS, options)
 		var $links = this.$element.find('a')
 		$links.on('click', this.toggle)
 
 		if(this.$element.hasClass('tree-default')) this.addIcon($links)
+		if(options.draggable) this.enableDraggable($links);
+		if(options.droppable) this.enableDroppable($links);
+	}
+
+	Tree.DEFAULTS = {
+		draggable: false,
+		dragstart: dragstart,
+		dragover: dragover,
+		droppable: false,
+		drop: drop,
+	}
+
+	function dragstart(event){
+		event.target.id = "tree-drag-temp-id"
+	}
+
+	function dragover(event){
+		if($(event.target).siblings('ul').length != 0) {
+			event.preventDefault()
+		}
+	}
+
+	function drop(event){
+		var $draggable = $('#tree-drag-temp-id')
+		$draggable.removeAttr('id')
+		var $droppable = $(event.target)
+		if($droppable.parent()[0] !== $draggable[0]) {
+			$droppable.siblings('ul').append($draggable)
+		}
 	}
 
 	Tree.prototype.toggle = function(e) {
@@ -25,12 +55,30 @@
 
 	};
 
-	$.fn.tree = function () {
+	Tree.prototype.enableDraggable = function($links) {
+		var self = this
+		$links.each(function(){
+			$(this).parent()
+			.attr('draggable', true)
+			.on('dragstart', self.options.dragstart)
+		})
+	};
+
+	Tree.prototype.enableDroppable = function($links) {
+		var self = this
+		$links.each(function(){
+			$(this)
+			.on('dragover', self.options.dragover)
+			.on('drop', self.options.drop)
+		})
+	};
+
+	$.fn.tree = function (options) {
 		return this.each(function () {
 			var $this = $(this)
 			var data = $this.data('bsx.tree')
 
-			if (!data) $this.data('bsx.tree', (data = new Tree(this)))
+			if (!data) $this.data('bsx.tree', (data = new Tree(this, options)))
 		})
 	}
 
