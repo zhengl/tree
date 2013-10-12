e69225fc9a171aa315d822b4cefc28b1fcf8a493
Zheng Li <loylizh@gmail.com>
2013-10-12 11:21:50 +0800

    Add icons to tree-default

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,4 +20,6 @@ module.exports = function(grunt){
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['less']);
 };
--- js/tree.js
+++ js/tree.js
@@ -1,11 +1,28 @@
 +function ($) { "use strics";
 
 	var Tree = function (element) {
-		$(element).find('a').on('click', this.toggle)
+		this.$element = $(element)
+		var $links = this.$element.find('a')
+		$links.on('click', this.toggle)
+
+		if(this.$element.hasClass('tree-default')) this.addIcon($links)
 	}
 
 	Tree.prototype.toggle = function(e) {
-		$(e.target).siblings().toggleClass('collapse')
+		var $a = $(e.target)
+		if($a.siblings('ul').length != 0) $a.parent().toggleClass('tree-collapse')
+	};
+
+	Tree.prototype.addIcon = function($links) {
+		$links.each(function(){
+			var $this = $(this)
+			var $fileIcon = $('<span class="glyphicon glyphicon-file"></span>')
+			var $openIcon = $('<span class="glyphicon glyphicon-folder-open"></span>')
+			$this.html(" " + $this.html()) //dirty hack to prepend padding
+			if($this.siblings('ul').length == 0) $this.prepend($fileIcon)
+			else $this.prepend($openIcon)
+		})
+
 	};
 
 	$.fn.tree = function () {
